763f54358b29b8a7c929956d4a0b25d48933ca4c
森重翔平 <morishigeshouhei@morishigeshouheinoMacBook-Pro.local>
2017-04-15 19:16:23 +0900

    なんか色々

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -32,6 +32,11 @@ class ViewController: UIViewController {
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     let resultViewController:ResultViewController = segue.destination as! ResultViewController
     resultViewController.imageCount = imageCount
+    self.timer.invalidate()
+    self.timer = nil
+    movebtn.setTitle("再生", for: UIControlState.normal)
+    susumu.isEnabled = true
+    modoru.isEnabled = true
   }
 
 
